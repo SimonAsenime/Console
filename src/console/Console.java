8fa1802a313e4f5875bb300036013ed3eb56d871
simon <40150870+TeryVeneno@users.noreply.github.com>
2020-07-28 20:57:33 -0500

    Console Release Version 1.0

--- src/console/Console.java
+++ src/console/Console.java
@@ -97,7 +97,7 @@ public class Console implements KeyListener {
         scrollPane.setPreferredSize(new Dimension(container.getWidth(),
                 text_area.getFontMetrics(text_area.getFont()).getHeight()*count_lines()+text_field.getHeight()));
         scrollPane.getVerticalScrollBar().setValue(text_area.getHeight()-
-                text_area.getFontMetrics(text_area.getFont()).getHeight()*4);
+                text_area.getFontMetrics(text_area.getFont()).getHeight()*8);
     }
 
     public void out (String message) {
@@ -112,7 +112,7 @@ public class Console implements KeyListener {
         scrollPane.setPreferredSize(new Dimension(container.getWidth(),
                 text_area.getFontMetrics(text_area.getFont()).getHeight()*count_lines()+text_field.getHeight()));
         scrollPane.getVerticalScrollBar().setValue(text_area.getHeight()-
-                text_area.getFontMetrics(text_area.getFont()).getHeight()*4);
+                text_area.getFontMetrics(text_area.getFont()).getHeight()*8);
     }
 
     public String get_input () throws InterruptedException {
